1bc4effaa36571bb20615d24503dfa34860e5a84
SANAD BANSAL <sanadwork240@gmail.com>
2026-07-15 18:17:43 +0530

    sql_analysis

--- Data_analysis_codes/analysis.sql
+++ Data_analysis_codes/analysis.sql
@@ -38,3 +38,52 @@ SELECT
 FROM customer_shopping_behavior
 GROUP BY category
 ORDER BY total_revenue DESC;
+
+
+--WHICH ITEM GENRATED MORE REVENUE
+SELECT
+    item_purchased,
+    SUM(purchase_amount_usd) AS total_revenue
+FROM customer_shopping_behavior
+GROUP BY item_purchased
+ORDER BY total_revenue DESC
+LIMIT 10;
+
+--Which locations generate the highest total revenue?
+SELECT
+    location,
+    SUM(purchase_amount_usd) AS total_revenue
+FROM customer_shopping_behavior
+GROUP BY location
+ORDER BY total_revenue DESC
+LIMIT 10;
+
+--REVENUE BY AGE GROUPS CREATED
+SELECT
+    age_group,
+    SUM(purchase_amount_usd) AS total_revenue
+FROM customer_shopping_behavior
+GROUP BY age_group
+ORDER BY total_revenue DESC;
+
+--How does sales performance differ by gender?
+SELECT
+    gender,
+    COUNT(*) AS total_orders,
+    SUM(purchase_amount_usd) AS total_revenue,
+    ROUND(AVG(purchase_amount_usd), 2) AS avg_purchase_amount
+FROM customer_shopping_behavior
+GROUP BY gender
+ORDER BY total_revenue DESC;
+
+--Subscription Analysis
+SELECT 
+   subscription_status,
+   COUNT(*) AS TOTAL_SUBSCRIBERS,
+    SUM(purchase_amount_usd) AS total_revenue,
+    ROUND(AVG(purchase_amount_usd), 2) AS avg_purchase_amount
+FROM customer_shopping_behavior
+GROUP BY subscription_status
+ORDER BY total_revenue DESC;
+
+
